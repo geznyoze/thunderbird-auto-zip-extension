2fac1137e467d9618c9c52856a1e1424699509c4
Fulop Windisch <windisch.fulop@gmail.com>
2026-07-11 22:53:11 +0200

    fixed dialog message

--- dialog.js
+++ dialog.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const settings = await browser.storage.local.get({ sizeLimitMB: 3 });
+  document.getElementById('sizeLimit').textContent = `${settings.sizeLimitMB}`;
+});
+
 document.getElementById('yesBtn').addEventListener('click', () => {
   browser.runtime.sendMessage({ action: "user_choice", zip: true });
   window.close();
